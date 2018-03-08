cb40fc714ec1528d01e412e539fad9c5750445a7
Filipe Reis <epilif.sr@gmail.com>
2018-03-08 00:52:19 -0300

    build.gradle (Projeto e app) > Adicionando dependencias de configuracao Firebase; Cadastro > Cadastrando novo usuario com firebase; MainActivity > Autenticacao; Principal > Nova tela apos usuario logar; xmls > Adpatando layouts; AndroidManifest > Cadastro da nova activity (Principal); google-services.json > Arquivo de configuracao do firebase;

--- app/src/main/java/br/iesb/mobile/alunoonline/Cadastro.java
+++ app/src/main/java/br/iesb/mobile/alunoonline/Cadastro.java
@@ -1,22 +1,40 @@
 package br.iesb.mobile.alunoonline;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Cadastro extends AppCompatActivity {
 
     private TextView login;
+    private EditText editNome, editSobrenome, editSenhaCad, editEmailCad, editTelefone;
+    private Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
-        login = (TextView) findViewById(R.id.textView9);
+        login           = findViewById( R.id.textFazerLogin );
+        editNome        = findViewById( R.id.editNome       );
+        editSobrenome   = findViewById( R.id.editSobrenome  );
+        editSenhaCad    = findViewById( R.id.editSenhaCad   );
+        editEmailCad    = findViewById( R.id.editEmailCad   );
+        editTelefone    = findViewById( R.id.editTelefone   );
+        btnRegistrar    = findViewById( R.id.btnRegistrar   );
 
+        //Volta para tela de login
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,5 +42,31 @@ public class Cadastro extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //Cadastra novo usuario
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cadastrar();
+            }
+        });
+    }
+
+    private void cadastrar() {
+        FirebaseAuth cadastro = FirebaseAuth.getInstance();
+
+        final Task<AuthResult> processo = cadastro.createUserWithEmailAndPassword(editEmailCad.getText().toString(), editSenhaCad.getText().toString());
+        processo.addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(processo.isSuccessful()){
+                    Intent intent = new Intent(Cadastro.this, Principal.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(Cadastro.this, "Cadastro inválido!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/br/iesb/mobile/alunoonline/MainActivity.java
+++ app/src/main/java/br/iesb/mobile/alunoonline/MainActivity.java
@@ -1,24 +1,40 @@
 package br.iesb.mobile.alunoonline;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView esqueceuSenha, cadastro;
+    private Button loginBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        esqueceuSenha = (TextView) findViewById(R.id.textView10);
-        cadastro      = (TextView) findViewById(R.id.textView12);
+
+        //Localizando Componentes
+        esqueceuSenha = findViewById(R.id.textEsqueceuSenha );
+        cadastro      = findViewById(R.id.textCadastro      );
+        loginBtn      = findViewById(R.id.btnLogin          );
+
 
         esqueceuSenha.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, RecuperarSenha.class);
@@ -26,11 +42,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        cadastro.setOnClickListener(new View.OnClickListener(){
+        cadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Cadastro.class);
                 startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+
+        });
+    }
+
+    private void login() {
+        //Autencicacao Firebase
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        //Recuperando campos de login
+        EditText editEmail = findViewById(R.id.editEmail);
+        EditText editSenha = findViewById(R.id.editSenha);
+
+        //Metodo assincrono
+        Task<AuthResult> processo = auth.signInWithEmailAndPassword(editEmail.getText().toString(), editSenha.getText().toString());
+        processo.addOnCompleteListener(new OnCompleteListener<AuthResult>() { //Metodo que espera o resultado da autenticacao
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(MainActivity.this, Principal.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "E-mail ou senha inválido!", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
