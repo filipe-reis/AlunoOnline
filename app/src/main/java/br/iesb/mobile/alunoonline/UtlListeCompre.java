b3dd83c13fe8b8207f6072e9f4ca281d75fd0377
Filipe Reis <epilif.sr@gmail.com>
2018-05-26 15:19:49 -0300

    Acrescentando listagem das compras por lista selecionada ListaAdapater > Click no item; ListaListas > Seta o click do recycler view

---
+++ app/src/main/java/br/iesb/mobile/alunoonline/UtlListeCompre.java
@@ -0,0 +1,46 @@
+package br.iesb.mobile.alunoonline;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UtlListeCompre {
+
+    public UtlListeCompre(){};
+
+    public Object quickSort(Produtos[] prod, int inicio, int fim) {
+
+            if (inicio < fim) {
+                int posicaoPivo = separar(prod, inicio, fim);
+                quickSort(prod, inicio, posicaoPivo - 1);
+                quickSort(prod, posicaoPivo + 1, fim);
+            }
+
+            return Arrays.asList(prod);
+
+    }
+
+    private static int separar(Produtos[] vetor, int inicio, int fim) {
+
+        Produtos aux = vetor[inicio]; //Variavel utilizada na ultima parte do algoritmo para
+                                      // setar valor final do vetor
+        double pivo = vetor[inicio].getPreco();
+        int i = inicio + 1, f = fim;
+        while (i <= f) {
+            if (vetor[i].getPreco() <= pivo)
+                i++;
+            else if (pivo < vetor[f].getPreco())
+                f--;
+            else {
+                Produtos troca = vetor[i];
+                vetor[i] = vetor[f];
+                vetor[f] = troca;
+                i++;
+                f--;
+            }
+        }
+        vetor[inicio] = vetor[f];
+        vetor[f] = aux;
+        return f;
+    }
+
+}
