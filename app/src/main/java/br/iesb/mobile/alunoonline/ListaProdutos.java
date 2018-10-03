1ed1f6c629a1abe8fbc4c396eb353c7c4e72c7e3
Filipe <epilif.sr@gmail.com>
2018-10-03 07:56:58 -0300

    Conectando com a API.

--- app/src/main/java/br/iesb/mobile/alunoonline/ListaProdutos.java
+++ app/src/main/java/br/iesb/mobile/alunoonline/ListaProdutos.java
@@ -1,6 +1,8 @@
 package br.iesb.mobile.alunoonline;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -20,7 +22,9 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.iesb.mobile.alunoonline.Model.Produto;
 import br.iesb.mobile.alunoonline.Model.Produtos;
+import br.iesb.mobile.alunoonline.util.UtlListeCompre;
 
 public class ListaProdutos extends AppCompatActivity implements ProdutoRecyclerClickListener {
 
@@ -29,9 +33,10 @@ public class ListaProdutos extends AppCompatActivity implements ProdutoRecyclerC
     private FirebaseDatabase database;
     private DatabaseReference listaProdutosReference;
     private String nome_lista;
+    private ProgressDialog load;
+    private UtlListeCompre utl;
 
-
-    private List<Produtos> listaProdutos = new ArrayList<>();
+    private List<Produto> listaProdutos = new ArrayList<>();
     private ListaCompras listaCompra = new ListaCompras();
 
 
@@ -39,6 +44,8 @@ public class ListaProdutos extends AppCompatActivity implements ProdutoRecyclerC
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_produtos);
+
+        GetJson consomeAPI = new GetJson();
 
         Intent it = getIntent();
         nome_lista = it.getStringExtra("nome");
@@ -53,40 +60,81 @@ public class ListaProdutos extends AppCompatActivity implements ProdutoRecyclerC
         //Divide os itens da lista
         recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 
-        getListaProdutos();
+        //Executa chamada para buscar registros da API
+        consomeAPI.execute();
+
+//        getListaProdutos();
     }
 
-    private void getListaProdutos(){
+//    private void getListaProdutos(){
+//
+//        //Recuperar do banco de dados
+//        database = FirebaseDatabase.getInstance();
+//        listaProdutosReference = database.getReference().child("/Produtos");
+//
+//        listaProdutosReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                listaProdutos.clear();
+//                for(DataSnapshot c : dataSnapshot.getChildren()){
+//                    Produtos produto = new Produtos();
+//                    produto.setDesc( c.child("descricao").getValue().toString());
+//                    produto.setNome( c.child("produto")  .getValue().toString());
+//                    produto.setMarca(c.child("marca")    .getValue().toString());
+//                    produto.setPreco(Double.parseDouble(c.child("preco")    .getValue().toString().replace(',', '.')));
+//                    listaProdutos.add(produto);
+//                }
+//                recyclerViewAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                Toast.makeText(ListaProdutos.this, "Não foi possível recuperar os dados do banco de dados", Toast.LENGTH_LONG).show();
+//                Log.e("Erro aqui", databaseError.getMessage());
+//                Log.e("Detalhe do erro", databaseError.getDetails());
+//
+//            }
+//        });
+//    }
 
-        //Recuperar do banco de dados
-        database = FirebaseDatabase.getInstance();
-        listaProdutosReference = database.getReference().child("/Produtos");
 
-        listaProdutosReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                listaProdutos.clear();
-                for(DataSnapshot c : dataSnapshot.getChildren()){
-                    Produtos produto = new Produtos();
-                    produto.setDesc( c.child("descricao").getValue().toString());
-                    produto.setNome( c.child("produto")  .getValue().toString());
-                    produto.setMarca(c.child("marca")    .getValue().toString());
-                    produto.setPreco(Double.parseDouble(c.child("preco")    .getValue().toString().replace(',', '.')));
-                    listaProdutos.add(produto);
-                }
-                recyclerViewAdapter.notifyDataSetChanged();
+    private class GetJson extends AsyncTask<Void, Void, List<Produto>> {
+
+        @Override
+        protected void onPreExecute(){
+            load = ProgressDialog.show(ListaProdutos.this,
+                    "Por favor Aguarde ...", "Recuperando Informações do Servidor...");
+        }
+
+        @Override
+        protected List<Produto> doInBackground(Void... params) {
+            utl = new UtlListeCompre();
+
+            return utl.getInformacao("http://192.168.0.104:8080/produtos");
+        }
+
+        @Override
+        protected void onPostExecute(List<Produto> produtos){
+
+            listaProdutos.clear();
+            for (int i = 0; i < produtos.size(); i++){
+                Produto produto = new Produto();
+
+                //Preenche objeto produto
+                produto.setProduto_id(produtos.get(i).getProduto_id());
+                produto.setNome(produtos.get(i).getNome());
+                produto.setDescricao(produtos.get(i).getDescricao());
+                produto.setPreco(produtos.get(i).getPreco());
+                // ...
+
+
+                //Carrega lista rcyclerview
+                listaProdutos.add(produto);
             }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Toast.makeText(ListaProdutos.this, "Não foi possível recuperar os dados do banco de dados", Toast.LENGTH_LONG).show();
-                Log.e("Erro aqui", databaseError.getMessage());
-                Log.e("Detalhe do erro", databaseError.getDetails());
-
-            }
-        });
+            recyclerViewAdapter.notifyDataSetChanged();
+            load.dismiss();
+        }
     }
-
     public void chamaActivityCompras(){
         Intent it = new Intent(this, ListaCompras.class);
         startActivity(it);
---
+++ app/src/main/java/br/iesb/mobile/alunoonline/util/UtlListeCompre.java
@@ -0,0 +1,299 @@
+package br.iesb.mobile.alunoonline.util;
+
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import br.iesb.mobile.alunoonline.ListaCompras;
+import br.iesb.mobile.alunoonline.Model.Produto;
+import br.iesb.mobile.alunoonline.Model.Produtos;
+
+public class UtlListeCompre {
+
+    private double precoTotalLista;
+
+    public double getPrecoTotalLista() {
+        return precoTotalLista;
+    }
+
+    public void setPrecoTotalLista(double precoTotalLista) {
+        this.precoTotalLista = precoTotalLista;
+    }
+
+    public double getArredondado() {
+        return arredondado;
+    }
+
+    public void setArredondado(double arredondado) {
+        this.arredondado = arredondado;
+    }
+
+    private double arredondado;
+    public UtlListeCompre(){};
+
+    /**
+     * Faz ordenação por preco do produto utilizando o algoritmo quicksort
+     * @param prod vetor a ser ordenado
+     * @param inicio posicao inicial
+     * @param fim posicao final
+     * @return vetor ordenado pelo preco
+     */
+    public Object ordenarListaPreco(Produtos[] prod, int inicio, int fim) {
+
+            if (inicio < fim) {
+                int posicaoPivo = separarPorPreco(prod, inicio, fim);
+                ordenarListaPreco(prod, inicio, posicaoPivo - 1);
+                ordenarListaPreco(prod, posicaoPivo + 1, fim);
+            }
+            return Arrays.asList(prod);
+    }
+
+    private static int separarPorPreco(Produtos[] vetor, int inicio, int fim) {
+
+        Produtos aux = vetor[inicio]; //Variavel utilizada na ultima parte do algoritmo para
+        // setar valor final do vetor
+        double pivo = vetor[inicio].getPreco();
+        int i = inicio + 1, f = fim;
+        while (i <= f) {
+            if (vetor[i].getPreco() <= pivo)
+                i++;
+            else if (pivo < vetor[f].getPreco())
+                f--;
+            else {
+                Produtos troca = vetor[i];
+                vetor[i] = vetor[f];
+                vetor[f] = troca;
+                i++;
+                f--;
+            }
+        }
+        vetor[inicio] = vetor[f];
+        vetor[f] = aux;
+        return f;
+    }
+
+
+    /**
+     * Faz ordenação por preco do produto utilizando o algoritmo quicksort
+     * @param prod vetor a ser ordenado
+     * @param inicio posicao inicial
+     * @param fim posicao final
+     * @return vetor ordenado pelo preco
+     */
+    public Object ordenarListaNome(Produtos[] prod, int inicio, int fim) {
+
+        if (inicio < fim) {
+            int posicaoPivo = separarPorNome(prod, inicio, fim);
+            ordenarListaNome(prod, inicio, posicaoPivo - 1);
+            ordenarListaNome(prod, posicaoPivo + 1, fim);
+        }
+        return Arrays.asList(prod);
+    }
+
+    private static int separarPorNome(Produtos[] vetor, int inicio, int fim) {
+
+        Produtos aux = vetor[inicio]; //Variavel utilizada na ultima parte do algoritmo para
+        // setar valor final do vetor
+        String pivo = vetor[inicio].getNome();
+        int i = inicio + 1, f = fim;
+        while (i <= f) {
+            if (vetor[i].getNome().compareTo(pivo) <= 0 )
+                i++;
+            else if (vetor[f].getNome().compareTo(pivo) > 0 )
+                f--;
+            else {
+                Produtos troca = vetor[i];
+                vetor[i] = vetor[f];
+                vetor[f] = troca;
+                i++;
+                f--;
+            }
+        }
+        vetor[inicio] = vetor[f];
+        vetor[f] = aux;
+        return f;
+    }
+
+    /**
+     * Verifica os produtos da lista de compras estão disponiveis no mercado, retorna a lista com os produtos do mercado
+     * @param listaCompras lista das compras criada pelo usuario
+     * @param produtosMercado lista de produtos do mercado
+     * @return lista conciliada com os produtos disponiveis
+     */
+    public List<Produtos> conciliarListadeComprascomMercado(List<Produtos> listaCompras, List<Produtos> produtosMercado){
+        int i, j;
+        List<Produtos> listaConciliada = new ArrayList<>();
+
+        for (i = 0; i < listaCompras.size(); i++){
+            for(j = 0; j < produtosMercado.size(); j++){
+                if (listaCompras.get(i).getNome() == produtosMercado.get(j).getNome()){
+                    //Enontrou produto no mercado
+                    listaConciliada.add(produtosMercado.get(j));
+                }else{
+                    //Produto não encontrado no mercado
+                }
+            }
+        }
+        return listaConciliada;
+    }
+
+
+    public List<Produto> getInformacao(String end){
+        String json;
+        List<Produto> retorno;
+        json = UtlListeCompre.getJSONFromAPI(end);
+        Log.i("Resultado", json);
+        retorno = parseJson(json);
+
+        return retorno;
+    }
+
+    private List<Produto> parseJson(String json){
+        try {
+            List<Produto> produtos = new ArrayList<>();
+            JSONObject jsonObject;
+
+            //Atribui a string que contem o conteudo do json para um JSONArray
+            JSONArray jsonArray = new JSONArray(json);
+
+            for(int i = 0; i < jsonArray.length(); i++) {
+                Produto produto = new Produto();
+
+                //Para cada elemento do array, cria-se um JSONObject
+                jsonObject = jsonArray.getJSONObject(i);
+
+                //Atribui os objetos que estão nas camadas mais altas
+                produto.setProduto_id    (jsonObject.getString("produto_id"     ));
+                produto.setCodigo_barras (jsonObject.getString("codigo_barras"  ));
+                produto.setNome          (jsonObject.getString("nome"           ));
+                produto.setDescricao     (jsonObject.getString("descricao"      ));
+                produto.setPreco         (jsonObject.getDouble("preco"          ));
+
+                //Sku é um objeto, instancia um novo JSONObject
+                JSONObject sku = jsonObject.getJSONObject("sku");
+                produto.setSku           (sku.getString("sku"       ));
+                produto.setSku_descricao (sku.getString("descricao" ));
+                produto.setSku_id        (sku.getString("sku_id"    ));
+
+                //Categoria tambem é um Objeto
+                JSONObject categoria = jsonObject.getJSONObject("categoria");
+                produto.setCategoria_id        (categoria.getString("categoria_id" ));
+                produto.setCategoria_nome      (categoria.getString("nome"         ));
+                produto.setCategoria_descricao (categoria.getString("descricao"    ));
+
+                //Mercado tambem é um Objeto
+                JSONObject mercado = jsonObject.getJSONObject("mercado");
+                produto.setMercado_id        (mercado.getString("mercado_id" ));
+                produto.setMercado_nome      (mercado.getString("nome"       ));
+                produto.setMercado_descricao (mercado.getString("descricao"  ));
+                produto.setCnpj              (mercado.getString("cnpj"       ));
+
+
+                //Adicionando na lista
+                produtos.add(produto);
+            }
+
+            return produtos;
+        }catch (JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Faz conexão com a API
+     * Executa metodo GET no recurso passado na url e carrega o JSON
+     * @param url endereco que será feita a requisição
+     * @return
+     */
+    public static String getJSONFromAPI(String url){
+        String retorno = "";
+        try {
+            URL apiEnd = new URL(url);
+            int codigoResposta;
+            HttpURLConnection conexao;
+            InputStream is;
+
+            //Abre conexao utilizando protocolo HTTP e metodo GET
+            conexao = (HttpURLConnection) apiEnd.openConnection();
+            conexao.setRequestMethod("GET");
+
+            //Timeouts para conexao
+            conexao.setReadTimeout(15000);
+            conexao.setConnectTimeout(15000);
+            conexao.connect();
+
+            //Toda resquisição que não da erro retorna abaixo de 400 == HTTP_BAD_REQUEST
+            codigoResposta = conexao.getResponseCode();
+            if(codigoResposta < HttpURLConnection.HTTP_BAD_REQUEST){
+                is = conexao.getInputStream();
+            }else{
+                is = conexao.getErrorStream();
+            }
+
+            retorno = converterInputStreamToString(is);
+            is.close();
+            conexao.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return retorno;
+    }
+
+
+    /**
+     * Converte o InputStream recebido para string que conterá o JSON completo
+     * @param is
+     * @return
+     */
+    private static String converterInputStreamToString(InputStream is){
+        StringBuffer buffer = new StringBuffer();
+        try{
+            BufferedReader br;
+            String linha;
+
+            br = new BufferedReader(new InputStreamReader(is));
+            while((linha = br.readLine())!=null){
+                buffer.append(linha);
+            }
+
+            br.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        Log.w("JSON", buffer.toString());
+
+        return buffer.toString();
+    }
+
+
+
+
+
+}
